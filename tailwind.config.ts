7c3ca01419b89da2a06dccbb8d48cfa3f27f59ba
Yassen Hassan <Yassenhassan151@gmail.com>
2026-06-20 12:47:52 +0000

    make it 7 seconds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,7 +81,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        marquee: 'marquee 15s linear infinite',
+        marquee: 'marquee 7s linear infinite',
       },
     },
   },
